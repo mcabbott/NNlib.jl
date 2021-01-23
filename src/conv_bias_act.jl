3a9654cfe7f18cfc9b8e106df60c2d7f80119084
Michael Abbott <me@escbook>
2021-01-23 13:29:13 +0100

    a sketch of gradient handling

--- src/conv_bias_act.jl
+++ src/conv_bias_act.jl
@@ -37,3 +37,74 @@ for N in (3, 4)
         end
     end
 end
+
+for sigma in [:identity, :relu, :tanh]
+    # For these activation functions, we can calculate the gradient using only the final y
+
+    @eval function ChainRulesCore.rrule(::typeof(conv_bias_act), x, w, cdims, b, σ::typeof($sigma); kw...)
+        y = conv_bias_act(x, w, cdims, b, σ; kw...)
+        function conv_bias_act_pullback(Δ)
+            # First you need to un-broadcast the activation??
+            Δσ = if σ === identity
+                colmajor(Δ) # copy it
+            elseif σ === relu
+                similar(Δ) .= y .> 0
+            elseif σ === tanh
+                # ??
+                error()
+            end
+
+            # Second, you need to project onto the bias vector
+            db = if b===false
+                DoesNotExist()
+            else
+                sum!(similar(b), Δσ) # probably needs some reshaping first
+            end
+
+            # Third, you work out the nontrivial bits
+            return (
+                NO_FIELDS,
+                @thunk(∇conv_data(Δσ, w, cdims, kw...)),
+                @thunk(∇conv_filter(x, Δσ, cdims, kw...)),
+                DoesNotExist(),
+                db,
+                DoesNotExist(),
+            )
+        end
+        return y, conv_bias_act_pullback
+    end
+
+end
+
+#=
+for conv in [:conv, :depthwiseconv]
+    local ∇conv_data, ∇conv_filter = Symbol.(:∇, conv, [:_data, :_filter])
+    conv_pullback, ∇conv_data_pullback = Symbol.([conv, ∇conv_data], :_pullback)
+
+    @eval function ChainRulesCore.rrule(::typeof($conv), x, w, cdims; kw...)
+        function $conv_pullback(Δ)
+            Δ = colmajor(Δ)
+            return (
+                NO_FIELDS,
+                @thunk($∇conv_data(Δ, w, cdims, kw...)),
+                @thunk($∇conv_filter(x, Δ, cdims, kw...)),
+                DoesNotExist(),
+            )
+        end
+        return $conv(x, w, cdims; kw...), $conv_pullback
+    end
+
+    @eval function ChainRulesCore.rrule(::typeof($∇conv_data), x, w, cdims; kw...)
+        function $∇conv_data_pullback(Δ)
+            Δ = colmajor(Δ)
+            return (
+                NO_FIELDS,
+                @thunk($conv(Δ, w, cdims, kw...)),
+                @thunk($∇conv_filter(Δ, x, cdims, kw...)),
+                DoesNotExist(),
+            )
+        end
+        return $∇conv_data(x, w, cdims; kw...), $∇conv_data_pullback
+    end
+end
+=#
